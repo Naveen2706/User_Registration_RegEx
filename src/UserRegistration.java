88039d24a753d691497a4b18186cd6bc86b54a84
Naveen2706 <naveen271996@gmail.com>
2021-07-14 20:38:15 +0530

    email validation add

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -11,12 +11,17 @@ public class UserRegistration {
         return Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", firstName);
     }
 
+    public boolean emailValidation(String email) {
+        return Pattern.matches("^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+$", email);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserRegistration person = new UserRegistration();
         String firstName = scanner.nextLine();
         String lastName = scanner.nextLine();
+        String email = scanner.nextLine();
         System.out.println("Welcome to User Registration Program");
-        System.out.println(person.firstNameValidation(lastName));
+        System.out.println(person.emailValidation(email));
     }
 }
